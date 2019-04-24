c3882223ac6e24b1882bca12f5d5b829c5c09e24
jshin83 <jen.kyongah.lee@gmail.com>
2019-04-24 11:50:08 -0700

    select options working beautifully

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 const creatureArray = [];
 const section = $('#photo-template');
 const select = $('select');
+let keywordFilter = '';
+const seen = [];
+
 const Creature = function(url, title, description, keyword, horns){
   this.url = url;
   this.title = title;
@@ -11,35 +14,64 @@ const Creature = function(url, title, description, keyword, horns){
   creatureArray.push(this);
 };
 
-
 // grab data and render
 $.get('./data/page-1.json', data => {
   data.forEach(obj => {
     new Creature(obj.image_url, obj.title, obj.description, obj.keyword, obj.horns);
   });
   renderPage();
+  renderSelectOptions();
 });
 
 // display function
 const renderPage = function(){
-  const seen = new Set();
   section.empty();
-  creatureArray.forEach(creature => {
-    section.append(
-      `<div>
-      <h2>${creature.title}</h2>
-      <ul>
-      <li><img src=${creature.url} alt=${creature.keyword}</li>
-      <li><p>${creature.description}</p></li>
-      </ul>
-      </div>
-      `
-    );
 
-    //create options for select - grab keywords
-    if(!seen.has(creature.keyword)) {
+  creatureArray.forEach(creature => {
+    //if keywordFilter is set, filter
+    if(keywordFilter) {
+      if(creature.keyword === keywordFilter) {
+        displayCreatureDetails(creature);
+      }
+    } else {
+      select.empty();
+      displayCreatureDetails(creature);
+    }
+
+  });
+  keywordFilter = '';
+  select.on('change', selectOptionHandler);
+};
+
+//function to render select options
+const renderSelectOptions = function() {
+  creatureArray.forEach(creature => {
+    if(!seen.includes(creature.keyword)) {
       select.append(`<option>${creature.keyword}</option>`);
-      seen.add(creature.keyword);
+      seen.push(creature.keyword);
     }
   });
+};
+
+//helper function to create our elements
+const displayCreatureDetails = function(creature) {
+  section.append(
+    `<div>
+    <h2>${creature.title}</h2>
+    <ul>
+    <li><img src=${creature.url} alt=${creature.keyword}</li>
+    <li><p>${creature.description}</p></li>
+    </ul>
+    </div>
+    `
+  );
+};
+
+//helper function to put event handler on select
+const selectOptionHandler = function() {
+  event.preventDefault();
+  console.log('SELECT value??? ', select[0].selectedOptions[0].value);
+  console.log('value??? ', select[0].options[select[0].options.selectedIndex].value);
+  keywordFilter = select[0].selectedOptions[0].value; //select[0].options.selectedOptions[0].value;
+  renderPage();
 };
